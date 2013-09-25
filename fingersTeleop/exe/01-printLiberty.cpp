0c2d60a8ea88e551507ff1eb37f0445d68fe42b5
gtracy <gtracy92@icloud.com>
2013-09-24 23:10:23 -0400

    commit

--- fingersTeleop/exe/01-printLiberty.cpp
+++ fingersTeleop/exe/01-printLiberty.cpp
@@ -46,7 +46,7 @@ Eigen::Vector3d matrixToEuler(Matrix3d& m) {
 }   
 
 /* ********************************************************************************************* */
-bool getLiberty(Eigen::VectorXd& config) {
+bool getLiberty(Eigen::VectorXd& config, Eigen::VectorXd& config2) {
 
 	// Get the data
 	int r = 0;
@@ -57,13 +57,25 @@ bool getLiberty(Eigen::VectorXd& config) {
 
 	// Set the values for the position
 	Somatic__Vector* s1 = l_msg->sensor1;
+	Somatic__Vector* s2 = l_msg->sensor2;
+
 	config << s1->data[0], -s1->data[1], -s1->data[2], 0.0, 0.0, 0.0;
+	config2 << s2->data[0], -s2->data[1], -s2->data[2], 0.0, 0.0, 0.0;
 
 	// Convert from a quaternion to rpy representation
+
+	//s1
 	Eigen::Quaternion <double> oriQ (s1->data[6], s1->data[3], s1->data[4], s1->data[5]);
 	Eigen::Matrix3d oriM = oriQ.matrix();
 	Eigen::Vector3d oriE = matrixToEuler(oriM);
 	config.bottomLeftCorner<3,1>() << -oriE[2], -oriE[1], oriE[0];
+
+	//s2
+	Eigen::Quaternion <double> oriQ2 (s2->data[6], s2->data[3], s2->data[4], s2->data[5]);
+	Eigen::Matrix3d oriM2 = oriQ2.matrix();
+	Eigen::Vector3d oriE2 = matrixToEuler(oriM2);
+	config2.bottomLeftCorner<3,1>() << -oriE2[2], -oriE2[1], oriE2[0];
+
 	return true;
 }
 
@@ -79,9 +91,33 @@ void run() {
 
 		// Get the liberty data
 		Eigen::VectorXd config = VectorXd::Zero(6);
+		Eigen::VectorXd config2 = VectorXd::Zero(6);
 		bool success = false;
-		while(!success) success = getLiberty(config);
-		cout << config.transpose() << endl;
+		while(!success) success = getLiberty(config, config2);
+		cout << "config: " << config.transpose() << endl;
+		
+		//sensor 1
+		Eigen::Matrix3d matrix = (Eigen::AngleAxis <double> (config(5), Eigen::Vector3d(0.0, 0.0, 1.0)) *
+					Eigen::AngleAxis <double> (config(4), Eigen::Vector3d(0.0, 1.0, 0.0)) *
+					Eigen::AngleAxis <double> (config(3), Eigen::Vector3d(1.0, 0.0, 0.0))).matrix();
+		cout << "matrix: \n" << matrix << "\n" << endl;
+	
+		//sensor 2
+		Eigen::Matrix3d matrix2 = (Eigen::AngleAxis <double> (config2(5), Eigen::Vector3d(0.0, 0.0, 1.0)) *
+					Eigen::AngleAxis <double> (config2(4), Eigen::Vector3d(0.0, 1.0, 0.0)) *
+					Eigen::AngleAxis <double> (config2(3), Eigen::Vector3d(1.0, 0.0, 0.0))).matrix();
+		cout << "matrix2: \n" << matrix2 << "\n" << endl;
+
+
+		//angle of sensor 1 relative to polhemus cube
+		Eigen::Vector3d localZ (matrix(0,2), matrix(1,2), matrix(2,2));
+		double angle = acos(localZ.dot(Eigen::Vector3d(0.0, 0.0, 1.0)));
+		cout << "angle: " << angle / M_PI * 180.0 << endl;
+
+		//angle of sensor 1 relative to sensor 2
+		Eigen::Vector3d sensor2Z (matrix2(0,2), matrix2(1,2), matrix2(2,2));
+		double angle2 = acos(sensor2Z.dot(Eigen::Vector3d(-matrix(0,2), -matrix(1,2), -matrix(2,2))));
+		cout << "angle2: " << angle2 / M_PI * 180.0 << endl;
 
 		// Free buffers allocated during this cycle
 		aa_mem_region_release(&somaticContext.memreg);	
